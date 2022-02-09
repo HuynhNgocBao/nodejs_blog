a711c109481e2fda1ec0c911d583fcd56cb26434
HuynhNgocBao <baohuynhxayda@gmail.com>
2022-02-09 12:14:27 +0700

    fix bugs

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(
         extended: true,
     }),
 );
-app.use(      express.json());
+app.use(             express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine(
     'hbs',
